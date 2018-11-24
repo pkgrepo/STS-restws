b4306d04d2553c650870a22955f739e262108aff
prashant <mikun.rcm@gmail.com>
2018-11-24 23:44:03 +0530

    writing imple

--- src/main/java/com/pkg/restws/PatientServiceImpl.java
+++ src/main/java/com/pkg/restws/PatientServiceImpl.java
@@ -1,8 +1,28 @@
 package com.pkg.restws;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import com.pkg.restws.model.Patient;
 
 @Service
 public class PatientServiceImpl implements PatientService {
-
+	
+	Map<Long,Patient> patients = new HashMap<>();
+	long currentId = 123;
+	
+	
+	public PatientServiceImpl() {
+		init();
+	}
+	
+	void init() {
+		Patient patient = new Patient();
+		patient.setId(currentId);
+		patient.setName("Prashant");
+		patients.put(patient.getId(), patient);
+	}
+	
 }
